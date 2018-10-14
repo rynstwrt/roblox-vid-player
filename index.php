f8d54e45b2da9f33c711f18974cd24f24e55e01b
Ryan Stewart <ryanstewartalex@gmail.com>
2018-10-14 00:24:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 				$r = ($color >> 16) & 0xFF;
 				$g = ($color >> 8) & 0xFF;
 				$b = $color & 0xFF;
-				array_push($colorvals, $r.' '.$g.' '.$b);
+				array_push($colorvals, array($r, $g, $b));
 			}
 		}
 		return $colorvals;
@@ -40,46 +40,28 @@
 	$fps = $GLOBALS['fps'];
 	$width = $GLOBALS['width'];
 	$height = $GLOBALS['height'];
-
-	if ($video) 
-	{
-		if (strpos($video, ".mp4") !== false or strpos($video, ".webm") !== false or strpos($video, ".ogv") !== false or strpos($video, ".gif") !== false or strpos($video, ".flv") !== false) 
-			{
-			
-			deleteLocalFiles();
-
-			$file = getcwd() . "/video.mp4";
-			$outputfiles = getcwd() . "/%d.png";
-			file_put_contents($file, fopen($video, 'r'));
-			
-			$cmd = sprintf("ffmpeg -i " . $file . " -vf scale=%dx%d,fps=%d", $width, $height, $fps);
-			$cmdfull = $cmd . ' ' . $outputfiles;
-			exec($cmdfull);
 		
-			$images = glob(getcwd() . "/[0-9]*.png");
-			natsort($images);
+	deleteLocalFiles();
 
-			foreach ($images as $image)
-			{
-				$colarray = getRGB($image);
-				foreach ($colarray as $col)
-				{
-					$val = $col."<br>";
-					echo $val;
-				}
-				echo "<br><br>";
-			}
-			deleteLocalFiles();
+	//download video
+	$file = getcwd() . "/video.mp4";
+	$outputfiles = getcwd() . "/%d.png";
+	file_put_contents($file, fopen($video, 'r'));
+	
+	//convert video to image sequence
+	$cmd = sprintf("ffmpeg -i " . $file . " -vf scale=%dx%d,fps=%d", $width, $height, $fps);
+	$cmdfull = $cmd . ' ' . $outputfiles;
+	exec($cmdfull);
 
-		}
-		else
-		{
-			echo "\"".$video."\" Is not one of the accepted video types!";
-		}
-	}
-	else
+	$images = glob(getcwd() . "/[0-9]*.png");
+	natsort($images);
+
+	foreach ($images as $image)
 	{
-		echo "Missing arguments!";
+		$colarray = getRGB($image);
+		echo json_encode($colarray);
 	}
+
+	deleteLocalFiles();
 
 ?>
